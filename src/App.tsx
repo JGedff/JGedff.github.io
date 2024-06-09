ebde42b2e58a429b18ef6750c7fa5f7b8dbee274
JGedff <jordimarmi1@gmail.com>
2024-06-09 19:11:33 +0200

    feat: add i18next

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,15 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
-import { ComponentTest } from './components/componentTest'
+import { useTranslation } from "react-i18next";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [t, i18n] = useTranslation("global")
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <ComponentTest />
+      {t('hello')}
+      <button onClick={() => i18n.changeLanguage('ca')}>CA</button>
+      <button onClick={() => i18n.changeLanguage('en')}>EN</button>
+      <button onClick={() => i18n.changeLanguage('es')}>ES</button>
     </>
   )
 }
--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,31 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 
+import { I18nextProvider } from "react-i18next";
+import i18next from "i18next";
+import global_ca from "./translations/ca/global.json";
+import global_en from "./translations/en/global.json";
+import global_es from "./translations/es/global.json";
+
+i18next.init({
+  interpolation: { escapeValue: false },
+  resources: {
+    ca: {
+      global: global_ca
+    },
+    en: {
+      global: global_en
+    },
+    es: {
+      global: global_es
+    },
+  }
+})
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <I18nextProvider i18n={i18next}>
+      <App />
+    </I18nextProvider>
+  </React.StrictMode>
 )
